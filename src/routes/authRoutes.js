c2d9b519c57ea6a7f9778fa24f8b5eb6e1026267
alirazaatish <atishaliraza>
2025-01-25 09:22:16 +0500

    jwt token use for authentication

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const joi = require('joi');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
+
+const SECRET_KEY = "secret_key_token";
 
 const { signupSchema , loginSchema }  = require('../schema/authSchema')
 
@@ -23,22 +26,57 @@ router.post('/signup', async(req, res, next)=>{
   }
 });
 
+const users = [
+  {email: "aliraza@gmail.com", password: "ali123"},
+  {email: "alishba@gmail.com", password: "alishba123"}
+]
+
 router.get('/login', (req, res, next)=>{
     const {error}= loginSchema.validate(req.body);
-    
+    const { email, password } = req.body
     if(error){
         console.log(error.details);
         return res.status(400).send('Valtidation Error During Login');
     }else{
-        const data= req.body;
-        Object.keys(data).forEach((key)=>{
-            console.log(key, data[key]);
-        })
-        res.status(200).send('User Sucessfully Login');
+        // const data= req.body;
+        // Object.keys(data).forEach((key)=>{
+        //     console.log(key, data[key]);
+        // })
+        // res.status(200).send('User Sucessfully Login');
+        console.log("User sucessfully validated!!");
     }
+
+    const user = users.find((u)=>{
+      return u.email === email && u.password===password;
+    })
+    console.log("Email and Password From Req:", email, password);
+    console.log("User: ", user)
+
+    if(!user){
+      return res.status(400).json({message: "Invalid email and password!"});
+    }
+    //CREATE JWT TOKEN
+    const token = jwt.sign(
+      {email: req.body.email , password: req.body.password},
+      SECRET_KEY,
+      {expiresIn: '1h'}
+    );
+
+    res.json({message: 'User Sucessfully Login With Token', token});
+});
+
+// router.get('/profile', )
+
+router.get('/profile', (req, res)=>{
+  const token = req.headers['authorization'];
+  console.log("Token", token);
+  if(!token){
+    return res.status(401).json({message: " Access Denied. No token Provide"})
+  }
+  else{
+    return res.status(200).json({message: "Acess granted!"})
+  }
 })
-
-
 
 
 module.exports = router;
